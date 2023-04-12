93e1ee56a6547503414a9c6fc780f4c7eec97581
Shantanu Sawant <ishanksawant22@gmail.com>
2023-04-12 15:24:38 +0530

    Updates

---
+++ precache-manifest.aa933c1d913f49c2a25d41fcf632f4cd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fe98e6b19e58dc691f0",
+    "url": "/ShantanuCreations/static/css/main.196e393c.chunk.css"
+  },
+  {
+    "revision": "9fe98e6b19e58dc691f0",
+    "url": "/ShantanuCreations/static/js/main.9fe98e6b.chunk.js"
+  },
+  {
+    "revision": "fa677b773aaf13c72ca8",
+    "url": "/ShantanuCreations/static/js/1.fa677b77.chunk.js"
+  },
+  {
+    "revision": "9640f41ed4047bbf4b05",
+    "url": "/ShantanuCreations/static/css/2.2bf7e5f4.chunk.css"
+  },
+  {
+    "revision": "9640f41ed4047bbf4b05",
+    "url": "/ShantanuCreations/static/js/2.9640f41e.chunk.js"
+  },
+  {
+    "revision": "0e1f4841ef1d3bc7575d",
+    "url": "/ShantanuCreations/static/js/runtime~main.0e1f4841.js"
+  },
+  {
+    "revision": "92cf611e3d9b30aeda2491df1b1c041b",
+    "url": "/ShantanuCreations/static/media/1.92cf611e.jpg"
+  },
+  {
+    "revision": "4d195327de553bdaa57afcbac84652eb",
+    "url": "/ShantanuCreations/static/media/website-development-and-design-1.4d195327.jpg"
+  },
+  {
+    "revision": "708ab73ef4febb38253bd752f49a01ed",
+    "url": "/ShantanuCreations/static/media/app.708ab73e.jpg"
+  },
+  {
+    "revision": "2a08c5716b6c68f53ebd4fcfc7493097",
+    "url": "/ShantanuCreations/static/media/android.2a08c571.png"
+  },
+  {
+    "revision": "da4204b5c9442bd93cc3059b36c64bf1",
+    "url": "/ShantanuCreations/static/media/digital.da4204b5.jpg"
+  },
+  {
+    "revision": "a0e1df0991c759f3b0ace2689c2805f5",
+    "url": "/ShantanuCreations/static/media/marketing.a0e1df09.png"
+  },
+  {
+    "revision": "ece3e47f0f4cac1db1c45646f937d82e",
+    "url": "/ShantanuCreations/static/media/software.ece3e47f.jpg"
+  },
+  {
+    "revision": "83cd8a4b171ea24566bfea8191a25fd1",
+    "url": "/ShantanuCreations/static/media/about.83cd8a4b.jpg"
+  },
+  {
+    "revision": "465c6cb9f7abd753d8be338fa6495d0d",
+    "url": "/ShantanuCreations/index.html"
+  }
+];
